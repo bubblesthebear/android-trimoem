ae4b3c51c5368f70410ef93b793db454adaba2c4
bubblesthebear <42428543+bubblesthebear@users.noreply.github.com>
2020-03-07 05:59:52 -0600

    Draft profile done

--- Grab_Packages.sh
+++ Grab_Packages.sh
@@ -5,52 +5,6 @@
 
 
 
-function test_adb
-{
-
-    #
-    # First check if there is already an IP connected, second if nothing is connected then ask if there is an IP known, third if no IP is known and no devices then error.
-
-    # Check if there is already an IP connected.
-    validip="(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
-    devicelist=$(adb devices)
-        if [[ $devicelist =~ $validip ]]; then # IP Found
-            noips=0;
-        else # No IP
-            noips=1;
-            if [[ $(wc -l <$devicelist) -lt 1 ]]; then # No devices found either
-                echo idk # Idk
-            fi
-        fi
-        
-
-
-    ## If the validip above stays then below is outdated.
-
-    echo Listing devices...
-    echo
-    adb devices
-    echo
-
-    echo Grabbing IP Address
-    echo
-    adbdevip=$(adb shell ip addr show wlan0 | grep -E -o -m1 "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | head -1) # Runs command to show device's WiFi configuration, then uses grep to narrow down only what matches IP addresses visually, then uses 'head -1' to lower it further to the very first match, as two IP addresses are actually on the same output line and so both will be displayed otherwise.
-
-    adb tcpip 5575
-    sleep 4 # Otherwise it will say "5575:5575" as an error
-
-    echo 
-    echo Device IP is $adbdevip
-    #echo What is the device IP?
-    #read adbdevidip
-
-    adbdevid=$adbdevip:5575
-    echo 
-    echo Connecting to $adbdevid
-    echo 
-    adb connect $adbdevid
-
-}
 
 function adb_connect
 {
